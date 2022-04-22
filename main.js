02da79b55757bfdc5ada7ba3583deb2915214633
mostalt <sergey.antonov@ton.vision>
2022-04-23 01:57:11 +0700

    add main button

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ window.addEventListener("load", function () {
 
   if (window.Telegram.WebApp) {
     window.Telegram.WebApp.ready();
+    Telegram.WebApp.MainButton.setParams({
+      text_color: "#fff",
+      text: "Main Button",
+      color: "#6a0dad",
+      is_active: true,
+      is_visible: true,
+    }).onClick(() => console.log("Main Button Clicked"));
     console.log("READY");
   } else {
     console.log("WebApp is undefined");
